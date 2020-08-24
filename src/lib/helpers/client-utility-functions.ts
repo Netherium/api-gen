19c956f995939ecd6f49d90b99573055aea8f96b
George <gerpis@gmail.com>
2020-08-25 01:21:03 +0300

    fix: wrong auto-import

--- src/lib/helpers/client-utility-functions.ts
+++ src/lib/helpers/client-utility-functions.ts
@@ -1,10 +1,9 @@
 import { UIField } from '../interfaces/ui-field.model';
 import { UINestedField } from '../interfaces/ui-nested-field.model';
-import { hasOwnProperty } from 'tslint/lib/utils';
 
 export const getDisplayProperty = (uiField: UIField | UINestedField): string => {
   switch (true) {
-    case hasOwnProperty(uiField, 'displayProperty'):
+    case uiField.hasOwnProperty('displayProperty'):
       return uiField.displayProperty;
     default:
       return '_id';
